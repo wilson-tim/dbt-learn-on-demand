b4b693af2f4b1194ce1669913e8daa62e7210b93
Tim Wilson <tgwilson243@gmail.com>
2021-02-18 11:17:50 +0000

    Packages section completed

--- macros/limit_data_in_dev.sql
+++
@@ -1,5 +0,0 @@
-{% macro limit_data_in_dev(column_name, dev_days_of_data=3) -%}
-    {% if target.name = 'dev '%}
-        where {{ column _name }} == dateadd('day', -{{ dev_days_of_data }}, current_timestamp)
-    {% endif % }
-{%- endmacro %}    
